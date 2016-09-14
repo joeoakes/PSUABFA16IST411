0bfb9ef425ecd869adf1fd8e9845b39a0446ba9d
Alan Hua <azh5318@psu.edu>
2016-09-14 18:32:12 -0400

    mods done

--- ConnectivityAndNotificationSystem/notification_team3.py
+++ ConnectivityAndNotificationSystem/notification_team3.py
@@ -1,1 +1,24 @@
+LITHIUMKWHPACKFULL = 100
+LITHIUMKWHPACKHIGH = 75
+LITHIUMKWHPACKMEDIUM = 50
+LITHIUMKWHPACKLOW = 25
+NOTIFYLEVEL = ''
 
+class notificationLevel:
+    def __init__(self,kwhLevel):
+        self.kwhLevel = 100
+
+    def notifykwhLevel(kwhLevel):
+        if kwhLevel <= LITHIUMKWHPACKLOW:
+            NOTIFYLEVEL = 'CRITICAL'
+            print (NOTIFYLEVEL)
+        elif kwhLevel <= LITHIUMKWHPACKMEDIUM:
+            NOTIFYLEVEL = 'HIGH'
+            print(NOTIFYLEVEL)
+        elif kwhLevel <= LITHIUMKWHPACKHIGH:
+            NOTIFYLEVEL = 'MEDIUM'
+            print(NOTIFYLEVEL)
+        elif kwhLevel <= LITHIUMKWHPACKFULL:
+            NOTIFYLEVEL = 'LOW'
+            print(NOTIFYLEVEL)
+## probabaly how a notification level is calculated for the battery
