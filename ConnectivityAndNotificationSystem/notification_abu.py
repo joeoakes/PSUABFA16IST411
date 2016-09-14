af6b4853ff55221e0fa0468eb7115e0d1e6214df
Abu Sakif <sakifabu@live.com>
2016-09-14 10:24:55 -0400

    modifications done

--- ConnectivityAndNotificationSystem/notification_abu.py
+++ ConnectivityAndNotificationSystem/notification_abu.py
@@ -1,27 +1,47 @@
 # Enter notification classes here 
 # define notification classes
+import hashlib
+
 class Connected_notification
 	def cell_connection (self,Cellular)
 		devicename=''		
-		if (self.Cellular == True)
+		if (self.Cellular == True):
 		print('The %s is now connected to the cellular network' % devicename)
-		else 
+		else :
 		print ('No Cellular device is connected at the moment')
 		return devicename
 	def blue_connection (self,Bluetooth)
 		devicename_phone=''
-		if(self.Bluetooth == True)
+		if(self.Bluetooth == True):
 		print('This %s is now connected via Bluetooth ' % devicename_phone )
-		else
+		else:
 		print ('No device connected. \n Please turn on bluetooth on your device.')
 		return devicename_phone
-	def usb_connection (self,USB)
-		device=''
-		if (self.USB == True)
-		print('The %s is now connected through USB' % device)
-		else
-		print ('No device is connected through USB')
-		return device
+	def usb_connection (self,fltirelow,frtirelow,rltirelow,rrtirelow)
+		Tire=''
+		''' Check tire pressure function has to be created'''
+		while (tirepressurelow == True )
+			if (self.fltirelow == True):
+			Tire = 'Front Left tire'
+			print('The %s needs air ' % Tire )
+			elif (self.frtirelow == True):
+                        Tire = 'Front Right tire'
+			print ('The %s needs air ' % Tire )
+			elif (self.rltirelow == True):
+                        Tire = 'Rear Left tire'
+                        print('The %s needs air ' % Tire )
+                        elif (self.rrtirelow == True):
+                        Tire = 'Rront Right tire'
+                        print ('The %s needs air ' % Tire )
+	def tms_notification(self,USB)
+                device=''
+                if (self.USB == True):
+                print('The %s is now connected through USB' % device)
+                else:
+                print ('No device is connected through USB')
+                return device
+	
+
 #probably how a connection notification class should look like
 """
 Notification properties
