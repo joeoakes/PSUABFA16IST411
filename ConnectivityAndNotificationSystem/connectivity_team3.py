b4730c713f152e992338097a0409ea9380dc7321
svwoodhouse <svwoodhouse@gmail.com>
2016-09-15 21:26:30 -0400

    add Syd

--- ConnectivityAndNotificationSystem/connectivity_team3.py
+++ ConnectivityAndNotificationSystem/connectivity_team3.py
@@ -1,2 +1,13 @@
 class ConnectToServer:
-  
+
+
+
+
+
+class Conectivity:
+
+def _init_(self,connected):
+#function for the intial connection
+
+def BACKUP_CONNECTION(self,connection):
+#function for a back up connection in the even the first connection fails 
