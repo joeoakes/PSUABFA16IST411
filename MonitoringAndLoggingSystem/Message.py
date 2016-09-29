d423ffcb5321a31c22b9e2c2c24cd983fdc84d05
Ivan Iakimenko <isirc10@gmail.com>
2016-09-28 20:16:02 -0400

    Updated to include timestamp

--- MonitoringAndLoggingSystem/Message.py
+++ MonitoringAndLoggingSystem/Message.py
@@ -1,21 +1,21 @@
 import json
+import time
 import hashlib
 
 '''
 Author: Ivan Iakimenko
-Ver: 3
+Ver: 1
 Description: This class stores all of the properties of a message and has methods to convert it to a json string, generate a checksum
 based on the payload, and check the generated payload against the one supplied at initialization. If no checksum is available
 at initialization, a checksum is automatically generated based on the current payload (this is useful for sending messages so 
 you don't have to compute the payload from whatever code you're sending it from)
 '''
 class Message:
-    def __init__(self, car_id, origin_id, destination_id, health_code, timestamp, time_to_live, payload, uuid='0', checksum='0'):
+    def __init__(self, car_id, origin_id, destination_id, health_code, time_to_live, payload, timestamp=0.0, uuid='0', checksum='0'):
         self.car_id = car_id
         self.origin_id = origin_id
         self.destination_id = destination_id
         self.health_code = health_code
-        self.timestamp = timestamp
         self.ttl = time_to_live
         self.payload = payload
 
@@ -28,6 +28,11 @@ class Message:
             self.uuid = self.GenerateUUID()
         else:
             self.uuid = uuid
+
+        if timestamp == 0.0:
+            self.timestamp = time.time()
+        else:
+            self.timestamp = timestamp
 
     #Returns string JSON representation of current message object based on message formatting standard
     def ConvertToJSON(self):
